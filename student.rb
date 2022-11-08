d2d49576451bb62c2a274b56310163d0a55859be
Dennis <vigehi2017@gmail.com>
2022-11-08 12:03:20 +0300

    Add person Teacher and student objects

---
+++ student.rb
@@ -0,0 +1,19 @@
+require './person'
+
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(age, name, classroom, parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+  def classrooms(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
+  end
+end
