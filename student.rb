c42a2f2f750084262f3516dbf008808d33645e01
Dennis <vigehi2017@gmail.com>
2022-11-08 17:04:31 +0300

    Add classroom boook and rental relationships

--- student.rb
+++ student.rb
@@ -13,4 +13,10 @@ class Student < Person
     "¯\(ツ)/¯"
     # rubocop:disable RuleByName
   end
+
+  def classrooms(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
+  end
+  
 end
