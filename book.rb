c32439856f02d53fade8495a1848108b68153c72
Dennis <vigehi2017@gmail.com>
2022-11-13 00:50:16 +0300

    .

--- book.rb
+++
@@ -1,9 +0,0 @@
-class Book
-  attr_accessor :title, :author, :rentals
-
-  def initialize(title:, author:)
-    @title = title
-    @author = author
-    @rentals = []
-  end
-end
--- check.rb
+++
@@ -1,8 +0,0 @@
-require './decorator'
-
-class TrimmerDecorator
-  def correct_name(name)
-    name = name[0...10] if name.length > 10
-    name.capitalize
-  end
-end
